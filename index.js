02369612e552bcafa8ad79adfdf56e662c1c0947
Tirapat Numsinvichietchai <mind1990@gmail.com>
2018-01-13 11:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deepestChild() {
 function increaseRankBy(n) {
   var parsed = parseInt(n);
   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < lis.length; i++) {
+  for (var i = 0; i < lis.length + 1; i++) {
     lis[i].innerHTML = (i + parsed).toString();
   }
 };
