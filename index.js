dd2200ac735c545bb76c33cdf64a1eeb500223f1
Tirapat Numsinvichietchai <mind1990@gmail.com>
2018-01-13 11:43:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div');
+};
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div div');
+};
+
+function increaseRankBy(n) {
+  var parsed = parseInt(n);
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i <= lis.length; i++) {
+    lis[i] = (i + parsed).toString();
+  }
+};
