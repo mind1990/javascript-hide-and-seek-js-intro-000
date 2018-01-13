9947b408fd064d257bc242edc714e55581576d83
Tirapat Numsinvichietchai <mind1990@gmail.com>
2018-01-13 11:54:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function increaseRankBy(n) {
   var parsed = parseInt(n);
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (var i = 0; i < lis.length; i++) {
-    return lis[i].innerHTML = i + parsed;
+    lis[i].innerHTML = (i + parsed).toString();
   }
 };
